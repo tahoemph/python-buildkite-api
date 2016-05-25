259e58dd166c8f09580b7cd75a16ef0384a3a5b7
Michael Hunter <mhunter@saymedia.com>
2016-05-24 22:44:51 -0700

    Add pypi configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'python-buildkite-api',
+      packages = ['python-buildkite-api'], # this must be the same as the name above
+      version = '0.0',
+      description = 'A wrapper for the buildkite API',
+      author = 'Michael Hunter',
+      author_email = 'tahoemph@gmail.com',
+      url = 'https://github.com/tahoemph/python-buildkite-api', # use the URL to the github repo
+      download_url = 'https://github.com/peterldowns/python-buildkite-api/tarball/0.1', # I'll explain this in a second
+      keywords = ['python', 'api', 'buildkite'], # arbitrary keywords
+      classifiers = [],
+)
